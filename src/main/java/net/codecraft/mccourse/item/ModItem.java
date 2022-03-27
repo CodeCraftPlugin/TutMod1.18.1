30af790e41495a1f8d99ab0443479ad7918d7dfb
CodeCraftPlugin <86466022+CodeCraftPlugin@users.noreply.github.com>
2022-03-27 19:46:24 +0530

    did the armor thing steel_layer thing

--- src/main/java/net/codecraft/mccourse/item/ModItem.java
+++ src/main/java/net/codecraft/mccourse/item/ModItem.java
@@ -3,10 +3,8 @@ package net.codecraft.mccourse.item;
 import net.codecraft.mccourse.MccourseMod;
 import net.codecraft.mccourse.item.custom.*;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.SwordItem;
-import net.minecraft.item.ToolMaterials;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -23,20 +21,33 @@ public class ModItem {
             new Item(new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL).food(ModFoodComponents.TURNIP)));
     public static final Item COAL_SLIVER = registerItem("coal_sliver",
             new Item(new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
-    //Tool
+    //Tools
     public static final Item STEEL_PICKAXE = registerItem("steel_pickaxe",
-            new ModPickaxeItem(ModToolMaterial.STEEL,-3,20f,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
+            new ModPickaxeItem(ModToolMaterial.STEEL,-3,5f,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
     public static final Item STEEL_AXE = registerItem("steel_axe",
-            new ModAxeItem(ModToolMaterial.STEEL,5  ,20f,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
+            new ModAxeItem(ModToolMaterial.STEEL,5  ,15f,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
     public static final Item STEEL_SHOVEL = registerItem("steel_shovel",
-            new ModShovelItem(ModToolMaterial.STEEL,-10,20f,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
+            new ModShovelItem(ModToolMaterial.STEEL,-10,0f,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
     public static final Item STEEL_HOE = registerItem("steel_hoe",
-            new ModHoeItem(ModToolMaterial.STEEL,-11,20f,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
+            new ModHoeItem(ModToolMaterial.STEEL,-11,0f,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
     public static final Item STEEL_SWORD = registerItem("steel_sword",
             new SwordItem(ModToolMaterial.STEEL,4,20f,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
-    public static final Item WOOD_PAXEL = registerItem("wood_paxel",new PaxelItem(ToolMaterials.WOOD,6,6,
-            new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
-
+    // Custom Tools
+    public static final Item STEEL_PAXEL = registerItem("steel_paxel",
+            new PaxelItem(ModToolMaterial.STEEL,15,1,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
+    //Armors
+    //Helmet
+    public static final Item STEEL_HELMET = registerItem("steel_helmet",
+            new ArmorItem(ModArmorMaterials.STEEL, EquipmentSlot.HEAD,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
+    //Chestplate
+    public static final Item STEEL_CHESTPLATE = registerItem("steel_chestplate",
+            new ArmorItem(ModArmorMaterials.STEEL, EquipmentSlot.CHEST,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
+    //Leggings
+    public static final Item STEEL_LEGGINGS = registerItem("steel_leggings",
+            new ArmorItem(ModArmorMaterials.STEEL, EquipmentSlot.LEGS,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
+    //Boots
+    public static final Item STEEL_BOOTS = registerItem("steel_boots",
+            new ArmorItem(ModArmorMaterials.STEEL, EquipmentSlot.FEET,new FabricItemSettings().group(ModItemGroup.TUTMODGENERAL)));
     //Methods
    private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(MccourseMod.MOD_ID, name),item);
